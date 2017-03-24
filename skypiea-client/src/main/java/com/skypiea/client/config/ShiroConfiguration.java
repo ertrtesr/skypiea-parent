d849bbeabcc74d3bf92c510ea545aab091352a56
黄文鉴 <631236933@qq.com>
2017-03-24 17:24:37 +0800

    修改pom文件

---
+++ skypiea-client/src/main/java/com/skypiea/client/config/ShiroConfiguration.java
@@ -0,0 +1,16 @@
+package com.skypiea.client.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 作者: huangwenjian
+ * 描述:
+ * 创建时间: 2017-03-24 16:23
+ */
+
+@Configuration
+public class ShiroConfiguration {
+
+    
+
+}
