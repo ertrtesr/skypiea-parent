6d68b37e12fc14427e5847ac8fd831b9c29dd540
黄文鉴 <631236933@qq.com>
2017-03-28 19:50:15 +0800

    完善controller逻辑

--- skypiea-client/src/main/resources/templates/script/controller/headController.js
+++ skypiea-client/src/main/resources/templates/script/controller/headController.js
@@ -3,11 +3,21 @@
     angular.module('app').controller('headController', ['$scope', function ($scope) {
 
         console.log('headController启动了');
-        $scope.username = '登录'
+        $scope.showUsername = false;
+        $scope.showLoginButton = true;
+        $scope.username = '';
 
-        $scope.$on('currentUser', function (event, userInfo) {
-            console.log(userInfo);
-            $scope.username = userInfo.username;
+        $scope.$on('responseData', function (event, data) {
+            console.log(data);
+            if (data.status == 200) {
+                $scope.username = data.data.username;
+                $scope.showUsername = true;             //显示用户名
+                $scope.showLoginButton = false;         //隐藏登录按钮
+            } else {
+                //用户token不存在的情况
+                $scope.showUsername = false;
+                $scope.showLoginButton = true;
+            }
         });
 
     }]);
--- skypiea-client/src/main/resources/templates/script/controller/homeController.js
+++ skypiea-client/src/main/resources/templates/script/controller/homeController.js
@@ -12,9 +12,8 @@
             //每次刷新主页都会进到这个控制器,在这里需要通过accessToken请求服务器获取用户数据,展示在页面上
             if (accessToken != null) {
                 homeService.getUserByToken(accessToken, function (resp) {
-                    console.log(resp);
-                    var userInfo = resp.data.data;
-                    $rootScope.$broadcast('currentUser', userInfo);
+                    var data = resp.data;
+                    $rootScope.$broadcast('responseData', data);
                 });
             }
 
